095406d12f46bcab37890aac90862d4378b16131
JoseDeFreitas <37962411+JoseDeFreitas@users.noreply.github.com>
2022-10-08 11:22:21 +0100

    Try with FastAPI

--- api/index.py
+++ api/index.py
@@ -1,19 +1,7 @@
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
+from fastapi import FastAPI
 
-class handler(BaseHTTPRequestHandler):
+app = FastAPI()
 
-	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
-
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
-		self.wfile.write(message.encode())
-		return
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
