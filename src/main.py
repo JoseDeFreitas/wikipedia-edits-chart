4333545cee400530a9809c0e7b235e9f465092a9
JoseDeFreitas <37962411+JoseDeFreitas@users.noreply.github.com>
2022-10-08 12:53:13 +0100

    Create method

--- src/main.py
+++ src/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "test"
+
+
+@app.get("/{username}/{language}")
+async def get_user(username: str, language: str):
+    return f"Hello, {username}. You are in the {language} Wikipedia"
